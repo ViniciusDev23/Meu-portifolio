c32dad01e85c69c22b655df593d044bc953cbea8
ViniciusDev23 <143198965+ViniciusDev23@users.noreply.github.com>
2023-10-17 13:21:54 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+let ul = document.querySelector('div .ul')
+
+function openMenu(){
+    ul.classList.add('open')
+}
+
+function closeMenu(){
+    ul.classList.remove('open')
+}
+document.getElementById("enviar").addEventListener("click", function() {
+    alert("Mensagem enviada, retornarei em breve!");
+    document.querySelector(".mob-form-500").reset(); // Opcional: redefinir o formulário após o alerta
+});
+
+
