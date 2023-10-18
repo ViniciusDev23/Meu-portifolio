5f9f2533f5b0cf36464c37f5c5201a23cfc5b131
ViniciusDev23 <143198965+ViniciusDev23@users.noreply.github.com>
2023-10-18 14:40:26 -0300

    atualizações portifolio

--- js/main.js
+++ js/main.js
@@ -11,5 +11,3 @@ document.getElementById("enviar").addEventListener("click", function() {
     alert("Mensagem enviada, retornarei em breve!");
     document.querySelector(".mob-form-500").reset(); // Opcional: redefinir o formulário após o alerta
 });
-
-
